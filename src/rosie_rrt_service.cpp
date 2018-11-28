309b244a64a6d49518fb97273171b20c1a10d6a8
TNeverWood <tobiasniewalda@web.de>
2018-11-28 16:56:27 +0100

    Fixed code after moving services

--- src/rosie_rrt_service.cpp
+++ src/rosie_rrt_service.cpp
@@ -1,14 +1,14 @@
 #include "ros/ros.h"
 #include "arduino_servo_control/SetServoAngles.h"
-#include <rosie_map_controller/RequestRerun.h>
-#include <rosie_map_controller/CommissionRerun.h>
-#include <rosie_map_controller/StartRRT.h>
+#include <rosie_path_finder/RequestRerun.h>
+#include <rosie_path_finder/CommissionRerun.h>
+#include <rosie_path_finder/StartRRT.h>
 
 #include <cstdlib>
 
 int rerun_command = 0; // no rerun
 
-bool RequestRerun(rosie_map_controller::RequestRerun::Request &req,rosie_map_controller::RequestRerun::Response &res){
+bool RequestRerun(rosie_path_finder::RequestRerun::Request &req,rosie_path_finder::RequestRerun::Response &res){
 
 	if(req.question == 0){
 		res.answer = rerun_command;
@@ -22,7 +22,7 @@ bool RequestRerun(rosie_map_controller::RequestRerun::Request &req,rosie_map_con
 
 }
 
-bool CommissionRerun(rosie_map_controller::CommissionRerun::Request &req, rosie_map_controller::CommissionRerun::Response &res){
+bool CommissionRerun(rosie_path_finder::CommissionRerun::Request &req, rosie_path_finder::CommissionRerun::Response &res){
 
 	if(req.command == 0){
 		rerun_command = 0;
@@ -40,7 +40,7 @@ bool CommissionRerun(rosie_map_controller::CommissionRerun::Request &req, rosie_
 }
 
 bool start_command = 0;
-bool StartPathPlanning(rosie_map_controller::StartRRT::Request &req, rosie_map_controller::StartRRT::Response &res){
+bool StartPathPlanning(rosie_path_finder::StartRRT::Request &req, rosie_path_finder::StartRRT::Response &res){
 	if(req.command == 1){
 		start_command = 1;
 		res.answer = 1;
@@ -58,9 +58,9 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   //ros::ServiceClient client = n.serviceClient<arduino_servo_control::SetServoAngles>("SetServoAngles");
-  ros::ServiceServer ReqService = n.advertiseService<rosie_map_controller::RequestRerun::Request, rosie_map_controller::RequestRerun::Response>("request_rerun", RequestRerun);
-	ros::ServiceServer CommService = n.advertiseService<rosie_map_controller::CommissionRerun::Request, rosie_map_controller::CommissionRerun::Response>("commission_rerun", CommissionRerun);
-	ros::ServiceServer StartService = n.advertiseService<rosie_map_controller::StartRRT::Request, rosie_map_controller::StartRRT::Response>("start_rrt", StartPathPlanning);
+  ros::ServiceServer ReqService = n.advertiseService<rosie_path_finder::RequestRerun::Request, rosie_path_finder::RequestRerun::Response>("request_rerun", RequestRerun);
+	ros::ServiceServer CommService = n.advertiseService<rosie_path_finder::CommissionRerun::Request, rosie_path_finder::CommissionRerun::Response>("commission_rerun", CommissionRerun);
+	ros::ServiceServer StartService = n.advertiseService<rosie_path_finder::StartRRT::Request, rosie_path_finder::StartRRT::Response>("start_rrt", StartPathPlanning);
   //ros::Subscriber sub = n.subscribe("MoveGates", 1000, GateCallback);
   ROS_INFO("Ready to do path planning.");
   ros::spin();
